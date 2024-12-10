62b8475aa7c794005af22d418f432dce5f9eaed7
son7du91 <sonlazack123456@gmail.com>
2024-12-10 21:58:33 +0700

    Add files via upload

---
+++ bt2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int soNguyen1 = 13;
+    int soNguyen2 = 4;
+    int tong = soNguyen1 + soNguyen2;
+    printf("Tong cua %d va %d la: %d\n", soNguyen1, soNguyen2, tong);
+    return 0;
+}
+    
