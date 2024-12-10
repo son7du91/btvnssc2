18adadd4fb892bfec76cd78eb353a87f7f1cf215
son7du91 <sonlazack123456@gmail.com>
2024-12-10 22:05:48 +0700

    Add files via upload

---
+++ bt4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int canhHinhVuong = 5;
+    int chuVi = 4 * canhHinhVuong;
+    int dienTich = canhHinhVuong * canhHinhVuong;
+    printf("Hinh vuong co canh = %d\n", canhHinhVuong);
+    printf("Chu vi hinh vuong = %d\n", chuVi);
+    printf("Dien tich hinh vuong = %d\n", dienTich);
+    return 0;
+}
