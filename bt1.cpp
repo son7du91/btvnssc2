b7c901c70663b4074c5c71361fa01a9c474f941a
son7du91 <sonlazack123456@gmail.com>
2024-12-10 21:54:41 +0700

    Add files via upload

---
+++ bt1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    // Kieu du lieu so nguyen
+    int soNguyen = 10;
+    // Kieu so thuc
+    float soThuc = 3.14
+    // Kieu ky tu
+    char kyTu = 'A';
+     printf("soNguyen: %d\n", soNguyen);
+     printf("soThuc: %.2f\n", soThuc);
+     printf("kyTu: %c\n", kyTu);
+     return 0;
+}
